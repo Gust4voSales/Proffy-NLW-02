8702c88c5acfdec160d7c874591bc94590b9d66c
Gustavo Sales <manoel0gustavo@gmail.com>
2020-08-03 12:32:51 -0300

    feat: WEB - add landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import  Landing from './pages/Landing';
 import './assets/styles/global.css';
 
 
 function App() {
   return (
-    <h1>OLA APP</h1>
+    <Landing />
   );
 }
 
