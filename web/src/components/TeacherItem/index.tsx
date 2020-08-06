9359c9fb7291dc69c789b53c2a5d00e3b9f7c87c
Gustavo Sales <manoel0gustavo@gmail.com>
2020-08-06 11:51:37 -0300

    feat: WEB - create new connection when user tries to connect to teacher

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import './styles.css';
+import api from '../../services/api';
 
 export interface Teacher {
     id: number;
@@ -17,6 +18,12 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+    
     return(
         <article className="teacher-item">
             <header>
@@ -37,7 +44,13 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
                     Preço/hora
                     <strong>R${teacher.cost}</strong>
                 </p>
-                <a href={`https://wa.me/${teacher.whatsapp}`} type="button">
+                <a 
+                    href={`https://wa.me/${teacher.whatsapp}`} 
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={createConnection} 
+                    type="button"
+                >
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
                 </a>
